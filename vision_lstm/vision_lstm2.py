ce12f23d135b099c785623bbb8222132ad587868
Nader Essam <nader.e.kassem@gmail.com>
2024-12-15 02:17:05 +0100

    Add DownSample and UpSample to the import modules

--- vision_lstm/vision_lstm2.py
+++ vision_lstm/vision_lstm2.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
-from .utils import interpolate_sincos, to_ntuple, VitPatchEmbed, VitPosEmbed2d, DropPath, SequenceConv2d
+from .utils import interpolate_sincos, to_ntuple, VitPatchEmbed, VitPosEmbed2d, DropPath, SequenceConv2d, DownSample, UpSample
 
 
 class SequenceTraversal(Enum):
